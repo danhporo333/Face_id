5a4b810536fc62266e6cf7b64fc244219f69c9f5
danhduong <danhzxc555@gmail.com>
2025-06-19 17:53:42 +0700

    update phân quyền cho từng api

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -74,50 +74,145 @@ router.delete("/deleteuser/:id", deleteUserController);
 router.post("/upload", uploadFile);
 
 //api khoa vien
-router.post("/createkhoa_vien", createKhoaVienController);
-router.get("/all", getAllKhoaVienController);
-router.put("/updatekhoa_vien", updateKhoaVienController);
-router.delete("/deletekhoa_vien/:makv", deleteKhoaVienController);
-router.post("/import-khoa-vien", importKhoaVienController);
+router.post(
+  "/createkhoa_vien",
+  auth,
+  checkRole(["ADMIN"]),
+  createKhoaVienController
+);
+router.get("/all", auth, checkRole(["ADMIN"]), getAllKhoaVienController);
+router.put(
+  "/updatekhoa_vien",
+  auth,
+  checkRole(["ADMIN"]),
+  updateKhoaVienController
+);
+router.delete(
+  "/deletekhoa_vien/:makv",
+  auth,
+  checkRole(["ADMIN"]),
+  deleteKhoaVienController
+);
+router.post(
+  "/import-khoa-vien",
+  auth,
+  checkRole(["ADMIN"]),
+  importKhoaVienController
+);
 
 //api class
-router.post("/createclass", createClassController);
-router.get("/allclass", getAllClassController);
-router.put("/updateclass", updateClassController);
-router.delete("/deleteclass/:malop", deleteClassController);
+router.post("/createclass", auth, checkRole(["ADMIN"]), createClassController);
+router.get("/allclass", auth, checkRole(["ADMIN"]), getAllClassController);
+router.put("/updateclass", auth, checkRole(["ADMIN"]), updateClassController);
+router.delete(
+  "/deleteclass/:malop",
+  auth,
+  checkRole(["ADMIN"]),
+  deleteClassController
+);
 
 //api student
-router.post("/createstudent", createStudentController);
-router.get("/allstudents", getAllStudentsController);
-router.put("/updatestudent", updateStudentController);
-router.delete("/deletestudent/:mssv", deleteStudentController);
-router.get("/student/:mssv", getStudentByMSSV);
+router.post(
+  "/createstudent",
+  auth,
+  checkRole(["ADMIN"]),
+  createStudentController
+);
+router.get(
+  "/allstudents",
+  auth,
+  checkRole(["ADMIN"]),
+  getAllStudentsController
+);
+router.put(
+  "/updatestudent",
+  auth,
+  checkRole(["ADMIN"]),
+  updateStudentController
+);
+router.delete(
+  "/deletestudent/:mssv",
+  auth,
+  checkRole(["ADMIN"]),
+  deleteStudentController
+);
+router.get("/student/:mssv", auth, checkRole(["ADMIN"]), getStudentByMSSV);
 
 //api mon hoc
-router.post("/createmonhoc", createMonHocController);
-router.get("/allmonhoc", getAllMonHocController);
-router.put("/updatemonhoc", updateMonHocController);
-router.delete("/deletemonhoc/:mamh", deleteMonHocController);
+router.post(
+  "/createmonhoc",
+  auth,
+  checkRole(["ADMIN"]),
+  createMonHocController
+);
+router.get("/allmonhoc", auth, checkRole(["ADMIN"]), getAllMonHocController);
+router.put("/updatemonhoc", auth, checkRole(["ADMIN"]), updateMonHocController);
+router.delete(
+  "/deletemonhoc/:mamh",
+  auth,
+  checkRole(["ADMIN"]),
+  deleteMonHocController
+);
 
 //api teacher
-router.post("/createteacher", createTeacherController);
-router.get("/allteachers", getAllTeachersController);
-router.put("/updateteacher", updateTeacherController);
-router.delete("/deleteteacher/:mgv", deleteTeacherController);
+router.post(
+  "/createteacher",
+  auth,
+  checkRole(["ADMIN"]),
+  createTeacherController
+);
+router.get(
+  "/allteachers",
+  auth,
+  checkRole(["ADMIN"]),
+  getAllTeachersController
+);
+router.put(
+  "/updateteacher",
+  auth,
+  checkRole(["ADMIN"]),
+  updateTeacherController
+);
+router.delete(
+  "/deleteteacher/:mgv",
+  auth,
+  checkRole(["ADMIN"]),
+  deleteTeacherController
+);
 
 //api room
-router.post("/createroom", createRoomController);
-router.get("/allrooms", getAllRoomsController);
-router.put("/updateroom", updateRoomController);
-router.delete("/deleteroom/:sop", deleteRoomController);
+router.post("/createroom", auth, checkRole(["ADMIN"]), createRoomController);
+router.get("/allrooms", auth, checkRole(["ADMIN"]), getAllRoomsController);
+router.put("/updateroom", auth, checkRole(["ADMIN"]), updateRoomController);
+router.delete(
+  "/deleteroom/:sop",
+  auth,
+  checkRole(["ADMIN"]),
+  deleteRoomController
+);
 
 //api tkb
-router.post("/createtkb", createTKBController);
-router.get("/alltkb", getAllTKBController);
-router.put("/updatetkb", updateTKBController);
-router.delete("/deletetkb/:id", deleteTKBController);
-router.get("/tkb/student/:mssv", getTKBByStudentController);
-router.post("/tkb/gan-sinh-vien", ganSinhVienVaoTKBController);
+router.post("/createtkb", auth, checkRole(["ADMIN"]), createTKBController);
+router.get("/alltkb", auth, checkRole(["ADMIN"]), getAllTKBController);
+router.put("/updatetkb", auth, checkRole(["ADMIN"]), updateTKBController);
+router.delete(
+  "/deletetkb/:id",
+  auth,
+  checkRole(["ADMIN"]),
+  deleteTKBController
+);
+router.get(
+  "/tkb/student/:mssv",
+  auth,
+  checkRole(["STUDENT"]),
+  getTKBByStudentController
+);
+router.post(
+  "/tkb/gan-sinh-vien",
+  auth,
+  checkRole(["ADMIN"]),
+  ganSinhVienVaoTKBController
+);
 router.post("/tkb/gan-lop", ganLopVaoTKBController);
 router.get(
   "/lich-hoc-ca-nhan",
